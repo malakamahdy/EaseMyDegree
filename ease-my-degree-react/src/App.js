aa3c3e2125f63fff8873fa89a842a871f2817d28
Malak Mahdy <malakmahdy@gmail.com>
2024-11-17 22:44:28 -0600

    Create an account takes you to register page

--- ease-my-degree-react/src/App.js
+++ ease-my-degree-react/src/App.js
@@ -1,19 +1,17 @@
-// src/App.js
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
-import Calculator from './Calculator'; // Import the Calculator page
+import Register from './Register'; // Assuming you have this page
+import Calculator from './Calculator'; // Assuming you have this page
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} /> {/* Login page */}
-          <Route path="/calculator" element={<Calculator />} /> {/* Calculator page */}
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* This is the root route */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
     </Router>
   );
 }
